a29b585083c7a826d9e202ffb9386d68759fa4a5
Luganda1 <62723878+Luganda1@users.noreply.github.com>
2020-08-24 23:02:51 -0700

    showing vs-code github setup

---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function todoItem() {
+    return (
+        <div>
+            hello from to do Todo Item
+        </div>
+    )
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from "./TodoItem"
+
+
+export default function TodoList() {
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <Item />
+        </div>
+    )
+}
